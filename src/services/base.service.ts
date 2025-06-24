f60db75e7ada38289498bb5a370d3bf7144c0996
Instilla-AI <service@instilla.it>
2025-06-24 12:13:06 +0200

    Delete src directory

--- src/services/base.service.ts
+++
@@ -1,9 +0,0 @@
-import { RailwayApiClient, railwayClient } from '@/api/api-client.js';
-
-export class BaseService {
-  protected client: RailwayApiClient;
-
-  constructor() {
-    this.client = railwayClient;
-  }
-} 
--- src/services/index.ts
+++
@@ -1,9 +0,0 @@
-export { databaseService } from './database.service.js';
-export { deploymentService } from './deployment.service.js';
-export { domainService } from './domain.service.js';
-export { projectService } from './project.service.js';
-export { serviceService } from './service.service.js';
-export { tcpProxyService } from './tcpProxy.service.js';
-export { templatesService } from './templates.service.js';
-export { variableService } from './variable.service.js';
-export { volumeService } from './volume.service.js'; 
