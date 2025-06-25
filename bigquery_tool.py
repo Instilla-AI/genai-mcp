77d73b791c5c4444ee17e7a14f2b067108a8511f
Instilla-AI <service@instilla.it>
2025-06-25 13:38:18 +0200

    Delete bigquery_tool.py

--- bigquery_tool.py
+++
@@ -1,6 +0,0 @@
-def bigquery_tool(query: str):
-    from langchain_google_genai import ChatGoogleGenerativeAI
-    from toolbox.tools.bigquery import BigQueryNL2SQLTool
-
-    tool = BigQueryNL2SQLTool(project_id="hip-nuova-folati")
-    return tool.run(query)
